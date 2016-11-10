7e7aaffc2d574f41865f410d73b719866a3c47ca
Marco Almeida <marcoalmeida@marcos-macbook-pro.local>
2016-11-10 12:13:49 -0500

    Initial Commit

---
+++ NOC-List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NOC-List
+//
+//  Created by Marco Almeida on 11/10/16.
+//  Copyright © 2016 THE IRON YARD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
